b5e1fb8b729ad54283b374542da94bcb0893c284
sameet2408 <sameet.samantaray24@gmail.com>
2025-05-05 04:58:50 -0700

    Update playwright-parallel.js
    Updated

--- playwright-parallel.js
+++ playwright-parallel.js
@@ -1,3 +1,5 @@
+import { test, expect } from '@playwright/test';
+
 const { chromium } = require('playwright')
 const {expect} = require("expect");
 const cp = require('child_process');
@@ -12,10 +14,19 @@ const parallelTests = async (capability) => {
 
   const page = await browser.newPage()
 
-
   try {
-    // Mark the test as completed or failed
- test('Test Scenario 1', async ({ page }) => {
+    
+    /* 1. OpenLambdaTest’sSeleniumPlaygroundfrom
+     https://www.lambdatest.com/selenium-playground
+     2. Click“SimpleFormDemo”.
+     3. ValidatethattheURLcontains“simple-form-demo”.
+     4. Createavariableforastringvaluee.g.:“WelcometoLambdaTest”.
+     5. Usethisvariabletoentervaluesinthe“EnterMessage”textbox.
+     6. Click“GetCheckedValue”.
+     7. Validatewhetherthesametextmessageisdisplayedintheright-hand
+     panelunderthe“YourMessage:”section. */
+    
+    test('Test Scenario 1', async ({ page }) => {
       await page.goto('https://www.lambdatest.com/selenium-playground');
       await page.getByRole('link',{name:"Simple Form Demo"}).click();
       await expect(page).toHaveURL(/simple-form-demo/);
@@ -23,9 +34,15 @@ const parallelTests = async (capability) => {
       await page.getByPlaceholder('Please enter your Message').fill(message);
       await page.getByRole('button', {name:"Get Checked Value"}).click();
       await expect(page.locator('#message')).toContainText(message);
-
-
- test('Test Scenario 2', async ({ page }) => {
+    
+    });
+    
+    /*1. Open the https://www.lambdatest.com/selenium-playground page and
+     click “Drag & Drop Sliders”.
+     2. Select the slider “Default value 15” and drag the bar to make it 95 by
+     validating whether the range value shows 95.*/ 
+    
+    test('Test Scenario 2', async ({ page }) => {
         await page.goto('https://www.lambdatest.com/selenium-playground');
     
     await page.getByText("Drag & Drop Sliders").click();
@@ -38,10 +55,22 @@ const parallelTests = async (capability) => {
            exactValue = await page.locator('#rangeSuccess').textContent(); 
         }
         await expect(page.locator('#rangeSuccess')).toHaveText('95'); 
-   
-
-
-test('Test Scenario 3', async ({ page }) => {
+    });
+    
+    /*Test Scenario 3:
+     1. Openthehttps://www.lambdatest.com/selenium-playground page and
+     click “Input Form Submit”.
+     2. Click “Submit” without filling in any information in the form.
+     3. Assert “Please fill in the fields” error message.
+     4. Fill in Name, Email, and other fields.
+     5. Fromthe Country drop-down, select “United States” using the text
+     property.
+     6. Fill in all fields and click “Submit”.
+     7. Oncesubmitted, validate the success message “Thanks for contacting
+     us, we will get back to you shortly.” on the screen*/
+    
+    
+    test('Test Scenario 3', async ({ page }) => {
       await page.goto('https://www.lambdatest.com/selenium-playground');
     
         await page.getByRole('link',{name:"Input Form Submit"}).click();
@@ -63,8 +92,11 @@ test('Test Scenario 3', async ({ page }) => {
         await page.getByRole ('textbox',{name:"Zip Code"}).fill('123456'); 
         await page.getByRole('button',{name:"Submit"}).click();
         await expect(page.getByText('Thanks for contacting us, we will get back to you shortly.')).toBeVisible();
+    });
     
-
+    // Mark the test as completed or failed
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Title matched' } })}`)
+    await teardown(page, browser)
   } catch (e) {
     await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: e.stack } })}`)
     await teardown(page, browser)
@@ -99,9 +131,9 @@ const capabilities = [
     'browserName': 'pw-firefox',
     'browserVersion': 'latest',
     'LT:Options': {
-      'platform': 'macOS Catalina',
+      'platform': 'MacOS Catalina',
       'build': 'Playwright With Parallel Build',
-      'name': 'Playwright Sample Test on macOS - firefox',
+      'name': 'Playwright Sample Test on Windows 8 - MicrosoftEdge',
       'user': process.env.LT_USERNAME,
       'accessKey': process.env.LT_ACCESS_KEY,
       'network': true,
